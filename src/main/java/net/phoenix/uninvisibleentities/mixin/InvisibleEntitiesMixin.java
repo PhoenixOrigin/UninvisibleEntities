ed3a74cd62d7325dd06ddf072199760f1513d406
phoenix <85183285+phoenixorigin@users.noreply.github.com>
2023-10-21 13:09:58 +0100

    compact + icon
    Signed-off-by: phoenix <85183285+PhoenixOrigin@users.noreply.github.com>

--- src/main/java/net/phoenix/uninvisibleentities/mixin/InvisibleEntitiesMixin.java
+++ src/main/java/net/phoenix/uninvisibleentities/mixin/InvisibleEntitiesMixin.java
@@ -1,8 +1,6 @@
 package net.phoenix.uninvisibleentities.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.scoreboard.AbstractTeam;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,24 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class InvisibleEntitiesMixin {
     @Shadow public abstract void setGlowing(boolean glowing);
     @Shadow protected abstract boolean getFlag(int index);
-    @Shadow @Nullable public abstract AbstractTeam getScoreboardTeam();
-
     @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
-    private void disableInvisibleEntities(CallbackInfoReturnable<Boolean> cir) {
-        if(this.getFlag(5)) {
-            this.setGlowing(true);
-        }
-        cir.setReturnValue(false);
-    }
-
+    private void disableInvisibleEntities(CallbackInfoReturnable<Boolean> cir) {if(this.getFlag(5)) this.setGlowing(true); cir.setReturnValue(false);}
     @Inject(method = "setInvisible", at = @At("HEAD"))
-    private void disableInvisibleEntities(boolean invisible, CallbackInfo ci) {
-        if(!invisible) this.setGlowing(false);
-    }
-
+    private void disableInvisibleEntities(boolean invisible, CallbackInfo ci) {if(!invisible) this.setGlowing(false);}
     @Inject(method = "getTeamColorValue", at = @At("HEAD"), cancellable = true)
-    private void getTeamColor(CallbackInfoReturnable<Integer> cir) {
-        if(this.getFlag(5)) cir.setReturnValue(0xfa4369);
-    }
+    private void getTeamColor(CallbackInfoReturnable<Integer> cir) {if(this.getFlag(5)) cir.setReturnValue(0xfa4369);}
 }
 
